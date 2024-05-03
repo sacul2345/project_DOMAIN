f531899cf0826a96512cab1aa0d56e391774c961
sacul2345 <lukesteinbrenner@gmail.com>
2024-05-03 17:06:54 -0500

    added an example scene

---
+++ input.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+var SPEED = 50
+
+func _physics_process(delta):
+	var vector = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		vector.y -= 1
+	if Input.is_action_pressed("move_down"):
+		vector.y += 1
+	if Input.is_action_pressed("move_left"):
+		vector.x -= 1
+	if Input.is_action_pressed("move_right"):
+		vector.x += 1
+	
+	movePlayer(vector * SPEED)
+	
+func movePlayer(vector):
+	#print("(" , vector.x , " , " , vector.y , ")")
+	apply_impulse(vector)
---
+++ platform.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+var platform : Node2D
+var ball : Node2D
+var maxSpeed : float = 400.0
+var AMPLIFY : int = 3
+
+func _ready():
+	platform = get_parent().get_node("Platform")
+	ball = get_parent().get_node("Ball")
+
+func _process(delta):
+	var distance = ball.position.x - platform.position.x
+	
+	if abs(distance) > 0.01:
+		var speed = min(maxSpeed, abs(distance)) * sign(distance)
+		print(speed, " , " , delta)
+		platform.position.x += (speed * delta) * AMPLIFY
